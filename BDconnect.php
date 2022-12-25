a6809e4ce2ce2c3b5c5f63144e60d9786d07ee24
MrCrazyRaven <ali.mamedov.6000@gmail.com>
2022-12-26 02:58:36 +0600

    Registratione

--- BDconnect.php
+++ BDconnect.php
@@ -1,11 +1,28 @@
 <?php
+
+$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+$surname = filter_var(trim($_POST['surname']), FILTER_SANITIZE_STRING);
+
+
+if(mb_strlen($login) < 5 || mb_strlen($login) > 90){
+	echo "Недопустимая длина логино";
+	exit();
+}
+if(mb_strlen($name) < 3 || mb_strlen($name) > 50){
+	echo "Недопустимая длина имени";
+	exit();
+}
+if(mb_strlen($surname) < 3 || mb_strlen($surname) > 50){
+	echo "Недопустимая длина фамилии";
+	exit();	
+}
 $connection = mysqli_connect('127.0.0.1','root','','test');
 if($connection == false)
 {
 	echo 'shfhff';
 	exit();
 }
-$result = mysqli_query($connection,"SELECT * FROM `test`");
-$r1 = mysqli_fetch_assoc($result);
-
+$connection->query("INSERT INTO `Person`(`Name`,`Surname`, `mail`) VALUES('$name','$surname','$login')");
+header('Location: /');
 ?>
--- index.php
+++ index.php
@@ -1,16 +1,15 @@
-<?php 
-require "BDconnect.php";
-?>
+
 <!DOCTYPE html>
 <html lang ="ru">
 
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="style.css" type="text/css"/>
+	<link rel="stylesheet" href="style1.css" type="text/css"/>
 	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+
 	<title>Курсовая работа </title>
 </head>
 <body>
@@ -32,11 +31,77 @@ require "BDconnect.php";
 	<div class="container">
 		<div class="intro__inner">	
 			<h1 class="intro__title">Вечер в хату</h1>
-		</div>		
+			
+		</div>
+			<div class="slidor">
+			<div class="slidor__item"><span class="slide__num">1</span>q</div>
+			<div class="slidor__item"><span class="slide__num">2</span>q</div>
+			<div class="slidor__item"><span class="slide__num">3</span>q</div>
+			</div>
 	</div>
 </div>
+  <section class="section">
+	<div class="container">
+		<div class="about">
+			<div class="about__item">
+				<div class="about__img">
+					<img src="Mesta1.jpg" alt="" width="360" height="500">
+				</div>
+				<div class="about__text">sfhshs</div>
+			</div>
+			<div class="about__item">
+				<div class="about__img">
+					<img src="Mesta2.jpeg" alt="" width="360" height="500">
+				</div>
+				<div class="about__text">shfsh</div>				
+			</div>
+			<div class="about__item">
+				<div class="about__img">
+					<img src="Mesta3.jpg" alt="" width="360" height="500">
+				</div>
+				<div class="about__text">sfhfhjjj</div>
+			</div>
+		</div>	
+	</div>
+  </section>
+	<section class="section">
+		<div class="container">
+			<div class="section_header">
+			<h3> Следующей представление</h3>
+			</div>
+		</div>
+	</section>
+  <div class="statistics">
+  <div class="container">
   
-
+		<div class="stat">
+			<div class="stat__item">9
+			<div class="stat__text">minutes</div>
+			</div>
+			<div class="stat__item">10
+			<div class="stat__text"> hours</div>
+			</div>
+			<div class="stat__item">25
+			<div class="stat__text">days</div>
+			</div>
+			<div class="stat__item">2022
+			<div class="stat__text"> years</div>
+			</div>
+		</div>
+		
+	</div>
+  </div>
+	<div class="container">
+		<div class="reg">
+		<h1>Форма регистрации</h1>
+		<form action="BDconnect.php" method="post">
+			<input type="text" class="form-control" name="name" id="name" placeholder="Ввидите имя"><br>
+			<input type="text" class="form-control" name="surname" id="surname" placeholder="Ввидите фамилию"><br>
+			<input type="text" class="form-control" name="login" id="login" placeholder="Ввидите логин"><br>
+			<button class="btn btn-success" type="submit">Заказать</button>
+		</form>
+		</div>
+	</div>
 </body>
 </html>
   
